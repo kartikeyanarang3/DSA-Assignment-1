e01a7e52314226cc53dddb885d369987bdd9dcc8
kartikeyanarang3 <kartikeyanarang3@gmail.com>
2026-02-22 19:59:53 +0530

    Update q1.py

--- q1.py
+++ q1.py
@@ -11,5 +11,20 @@ def fibonacci_naive(n):
     return n
   else:
     return fibonacci_naive(n-1) + fibonacci_naive(n-2)
+# The Time Complexity is O(2^n) because of 2 recursion calls for every term
+# The Space Complexity is O(n)
 
+#recursive fibonacci(memoized)
+def fibonacci_memoized(n) -> int:
+    global memo_fibonacci
+    if memo_fibonacci[n] != -1: 
+        return memo_fibonacci[n]
+    elif n <= 1: 
+        memo_fibonacci[n] = n
+        return n
+    else:
+        memo_fibonacci[n] = fibonacci_memoized(n-1) + fibonacci_memoized(n-2)
+        return memo_fibonacci[n]
+# The Time Complexity is O(n) because each term is only calculated once and memoized for future use
+# The Space Complexity is O(n)
 
